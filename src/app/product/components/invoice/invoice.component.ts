e0fd6e47eae8faefee9988a395c430a09a59f9f8
Donatus Okwe <braindona@gmail.com>
2025-06-04 04:47:26 -0700

    Payment method options

--- src/app/product/components/invoice/invoice.component.ts
+++ src/app/product/components/invoice/invoice.component.ts
@@ -1,6 +1,14 @@
 import { DatePipe } from '@angular/common';
-import { Component, computed, input, output, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  input,
+  model,
+  output,
+  signal,
+} from '@angular/core';
 import { Product } from '../product/product.component';
+import { FormsModule } from '@angular/forms';
 
 export interface InvoiceItem extends Pick<Product, '_id' | 'price' | 'name'> {
   quantity: number;
@@ -9,14 +17,19 @@ export interface InvoiceItem extends Pick<Product, '_id' | 'price' | 'name'> {
 
 @Component({
   selector: 'app-invoice',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.scss',
 })
 export class InvoiceComponent {
   invoiceId = signal<string>(`INV-${Math.floor(Math.random() * 1000000)}`);
   invoiceDate = signal<string>(new Date().toLocaleDateString());
-
+  paymentMethods = [
+    { label: 'Cash', value: 'Cash' },
+    { label: 'Card', value: 'Card' },
+    { label: 'Bank Transfer', value: 'Bank Transfer' },
+  ];
+  selectedPaymentMethod = model<string>('Cash');
   invoiceItems = input.required<InvoiceItem[]>();
   updateQuantity = output<InvoiceItem>();
   removeItem = output<string>();
@@ -40,5 +53,13 @@ export class InvoiceComponent {
     this.invoiceDate.set('');
   }
 
-  checkout() {}
+  checkout() {
+    console.log({
+      product: this.invoiceItems(),
+      totalAmount: this.totalAmount(),
+      paymentMethod: this.selectedPaymentMethod(),
+      invoiceId: this.invoiceId(),
+      invoiceDate: this.invoiceDate(),
+    });
+  }
 }
